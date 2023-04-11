8733ad59d9de866b17d584385af32fc4f87262f5
Alex Shabetia <alex.shabetia@gmail.com>
2023-04-11 19:08:12 +0300

    removed get

--- src/main/java/com/alexshabetia/samples/controller/SendMessageController.java
+++ src/main/java/com/alexshabetia/samples/controller/SendMessageController.java
@@ -3,8 +3,8 @@ package com.alexshabetia.samples.controller;
 import com.alexshabetia.samples.kafka.Producer;
 import com.alexshabetia.samples.kafka.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,23 +16,11 @@ import static com.vladkrava.converter.http.AbstractAvroHttpMessageConverter.AVRO
 public class SendMessageController {
 
     private final Producer producer;
-
-    @GetMapping("/send")
-    public ResponseEntity<User> send() {
-        var userMessage = User.newBuilder()
-                .setName("Jonh")
-                .setAge(33)
-                .build();
-        var topic = "user";
-
-        producer.send(topic, userMessage);
-
-        return ResponseEntity.ok(userMessage);
-    }
+    @Value("${app.topic}")
+    private String topic;
 
     @PostMapping(value = "/send", produces = AVRO_JSON, consumes = AVRO_JSON)
     public ResponseEntity<User> send(@RequestBody User user) {
-        var topic = "user";
 
         producer.send(topic, user);
 
